1b4c7bbb2a82c05cef43bd0c1e8ef61f29b565cb
Volkan Cirik <vcirik@mars.multicomp.cs.cmu.edu>
2022-03-10 12:50:17 -0500

    install is updated.

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ pip install -U transformers==3.2.0 torch==1.6.0
 pip install -U Cython==0.29.21
 pip install -U pytorch-pretrained-bert==0.6.2
 git clone https://github.com/pariajm/joint-disfluency-detector-and-parser.git; cd joint-disfluency-detector-and-parser; cd best_models; wget https://github.com/pariajm/joint-disfluency-detector-and-parser/releases/download/naacl2019/swbd_fisher_bert_Edev.0.9078.pt; cd ..; mkdir model && cd model; wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt; wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased.tar.gz; tar -xf bert-base-uncased.tar.gz
+cd data;  wget https://www.dropbox.com/s/4ce8ez7y2vrvdfi/wordvec.glove.tar.gz; tar xvfz wordvec.glove.tar.gz; rm wordvec.glove.tar.gz
